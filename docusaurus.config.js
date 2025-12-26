ce5dd30e87d216cc3fb85af3e4523fed793bd30c
shengbo.wang <shengbo.wang@trip.com>
2025-12-26 17:27:55 +0800

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://shengbo.wang.github.io',
+  url: 'https://shengbo-wang1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-website',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
